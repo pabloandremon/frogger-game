0f37f2f0b7eaf38d1f61229f8ed9b868815cae5e
pabloandremon <montes_117380@students.pupr.edu>
2021-05-16 18:34:28 -0400

    testing Pablo

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
 from Graphics import *
 
-#Settings for the window
+#Settings for the screen
 win = GraphWin("Frogger", 500, 700, autoflush=False)
 win.setBackground('Black')
 
-#Drawing the player here
+print("testing#4")
 
-
-#Creating the parent class
-class Stuff():
-    def __init__(self, x, y, width, height, image):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.image = image
-
-    #def WinMotion(self):
-
-
-#Creating the child class
-class Player(Stuff):
-    def __init__(self, x, y, width, height, image):
-        Stuff.__init__(self, x, y, width, height, image)
-
-
-#Creating the objects
-player = Player(0, -200, 40, 40, "frogger.gif")
