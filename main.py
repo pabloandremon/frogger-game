548e2d5b2b09e4b294f5d44b12edc407beafccbb
XadielF <82549794+XadielF@users.noreply.github.com>
2021-05-16 16:18:39 -0400

    class stuff add class player add

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 from Graphics import *
 
-#Settings for the screen
+#Settings for the window
 win = GraphWin("Frogger", 500, 700, autoflush=False)
 win.setBackground('Black')
-print('testing github')
+
+#Drawing the player here
+
+
+#Creating the parent class
+class Stuff():
+    def __init__(self, x, y, width, height, image):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = image
+
+    #def WinMotion(self):
+
+
+#Creating the child class
+class Player(Stuff):
+    def __init__(self, x, y, width, height, image):
+        Stuff.__init__(self, x, y, width, height, image)
+
+
+#Creating the objects
+player = Player(0, -200, 40, 40, "frogger.gif")
