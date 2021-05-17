83737b258d4bfecc6ff60d2fe11daef84a2176a3
XadielF <82549794+XadielF@users.noreply.github.com>
2021-05-17 19:31:15 -0400

    xadiel update may 17 7pm

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from graphics import *
 
 #Settings for the screen
-win = GraphWin("Frogger", 500, 700, autoflush=False)
-win.setBackground('Black')
+win = GraphWin("Frogger", 500, 700, autoflush=True)
 
-#drawing the player here
+#xadi testing motion
+car_right = []
+for x in [-1, 1]:
+    for y in [-1,1]:
+        circle = Circle(Point(250, 250), 20)
+        circle.draw(win)
+        circle.append((circle, (x, y)))
+
+for _ in range(250):
+    for circle, (x, y) in car_right:
+        circle.move(x, y)
+
 
 #Register the player
-win.Image("Frog.gif")
-win.Image("Car_left.gif")
-win.Image("Car_right.gif")
+#drawing the player here
+frog_img = Image(Point(200, 600) ,"Frog.gif")
+frog_img.draw(win)
+carL_img = Image(Point(100, 500),"Car_left.gif")
+carL_img.draw(win)
+carR_img = Image(Point(50, 400), "Car_right.gif")
+carR_img.draw(win)
 
-#Creating the parent class
+Creating the parent class
 class Stuff():
     def __init__(self, x, y, width, height, image):
         self.x = x
@@ -25,60 +39,31 @@ class Stuff():
         pen.draw(self.image)
         pen.update()
 
+
 #Creating the child class
-class Player(Stuff):
-    def __init__(self, x, y, width, height, image):
-        Stuff.__init__(self, x, y, width, height, image)
+class Player:
+    def __init__(self):
+        self.frog_img = char
+        self.win = window
+
+#Creating the objects 1
+    def create(self):
+        self.frog_img.setFill('green')
+        self.frog_img.draw(self.win)
     
-    def up(self):
-        self.y += 40
-    
-    def right(self):
-        self.x += 40
+    def Movement(self):
+        #Keyboard binding
+        if self.win.lastKey == "d":
+            self.p1.move(40, 0)
+        if self.win.lastKey == "a":
+            self.p1.move(-40, 0)
+        if self.win.lastKey == "s":
+            self.p1.move(0, 40)
+        if self.win.lastKey == "w":
+            self.p1.move(0, -40)
 
-    def down(self):
-        self.y -= 40
 
-    def left(self):
-        self.x -= 40
+#Creating the objects 2
 
-class Car(Stuff):
-    def __init__(self, x, y, width, height, image, dx):
-        Stuff.__init__(self, x, y, width, height, image)
-        self.dx = dx
-
-    def update(self):
-        self.x += self.dx
-        #Check borders of the game windows
-        if self.x < -400:
-            self.x = 400
-        
-        if self.x > 400:
-            self.x = -400
-
-#Creating the objects
-player = Player(0, -200, 40, 40, "frog.gif")
-car_left = Car(0, -150, 40, 121, "car_left.gif", -0.1)
-car_right = Car(0, -100, 40, 121, "car_right.gif", 0.1)
-
-#Keyboard binding
-win.getKey()
-win.checkKey(player.up, "Up")
-win.checkKey(player.down, "Down")
-win.checkKey(player.left, "Left")
-win.checkKey(player.right, "Right")
-
-while True:
-    #Render
-    player.undraw()
-    car_left.undraw()
-    car_right.undraw()
-    #Update the objects
-    car_left.update(30)
-    car_right.update(30)
-    #Update the screen
-    player.draw(win)
-    car_left.draw()
-    car_right.draw()
-
-update(30)
+#if frog_point == car_point
+#frog dead 
