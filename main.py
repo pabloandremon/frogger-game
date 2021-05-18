be8ad1dcb226cd3da8fbf0348c1d5f4a360041aa
XadielF <82549794+XadielF@users.noreply.github.com>
2021-05-18 17:56:39 -0400

    xadiel update, quitando cosas innecesarias y mejorando la estructura del code

--- main.py
+++ main.py
@@ -4,87 +4,74 @@ from graphics import *
 win = GraphWin("Frogger", 500, 700, autoflush=True)
 win.setBackground('black')
 
-#xadi testing motion
-car_right = []
-for x in [-1, 1]:
-    for y in [-1,1]:
-        circle = Circle(Point(250, 250), 20)
-        circle.draw(win)
-        car_right.append((circle, (x, y)))
+#Player Class
+class Player(object):
+    def __init__(self, startX, startY):
+        self.x = startX
+        self.y = startY
 
-for _ in range(250):
-    for circle, (x, y) in car_right:
-        circle.move(x, y)
-
-
-#Register the player
-#drawing the player here
-frog_img = Image(Point(200, 600) ,"frog.gif")
-frog_img.draw(win)
-carL_img = Image(Point(100, 500),"car_left.gif")
-carL_img.draw(win)
-carR_img = Image(Point(50, 400), "car_right.gif")
-carR_img.draw(win)
-
-#Creating the parent class
-class Stuff():
-    def __init__(self, x, y, width, height, image):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.image = image
-
-    def RenderMotion(self, pen):
-        pen.setCoords(self.x, self.y)
-        pen.draw(self.image)
-        pen.update()
-
-
-#Creating the child class
-class Player:
-    def __init__(self):
-        self.frog_img = char
-        self.win = window
-
-#Creating the objects 1
-    def create(self):
-        self.frog_img.setFill('green')
-        self.frog_img.draw(self.win)
+        self.frog = Image(Point(startX, startY), "frog.gif")
+        self.frog.draw(win)
     
+      #Keyboard binding
     def Movement(self):
-        #Keyboard binding
-        if self.win.lastKey == "d":
-            self.p1.move(40, 0)
-        if self.win.lastKey == "a":
-            self.p1.move(-40, 0)
-        if self.win.lastKey == "s":
-            self.p1.move(0, 40)
-        if self.win.lastKey == "w":
-            self.p1.move(0, -40)
+        if self.win.keys.get("d"):
+            self.frog.move(50, 0)
+        if self.win.keys.get("a"):
+            self.frog.move(-50, 0)
+        if self.win.keys.get("s"):
+            self.frog.move(0, 50)
+        if self.win.keys.get("w"):
+            self.frog.move(0, -50)
 
+#spawn player at 250, 650
+player_spawn = [Player(250, 650)]
+        
+#Enemy that moves created here
 class Enemy(object):
     def __init__(self, speed, startXpos, startYpos):
         self.speed = speed
         self.x = startXpos
         self.y = startYpos
 
-        self.graphic = Image(Point(startXpos, startYpos), "Car_left.gif")
-        self.graphic.draw(win)
+        self.car_right = Image(Point(startXpos, startYpos), "car_right.gif")
+
+        self.car_right.draw(win)
+        
 
     def move(self):
         self.x += self.speed
-        self.graphic.move(self.speed, 0)
+        self.car_right.move(self.speed, 0)
 
-myEnemies = [Enemy(40, 50, 100), Enemy(50, 50, 200), Enemy(60, 50, 300)]
+#Print enemy on different road lanes 
+enemy_line1 = [Enemy(30, 10, 600)] 
+enemy_line2 = [Enemy(30, 60, 560), Enemy(30, 200, 560)]
+enemy_line3 = [Enemy(30, 140, 520), Enemy(30, 250, 520), Enemy(30, 400, 520)]
 
-for i in range(40):  # main animation loop
-    for enemy in myEnemies:  # loop through the enemy list
+#Enemy animation
+for i in range(1000):  # main animation loop
+    for enemy in enemy_line1:  # loop through the enemy list
         enemy.move()
         time.sleep(1)  # wait a second...
+    
+    for enemy in enemy_line2:  
+        enemy.move()
+        time.sleep(1)  
+    
+    for enemy in enemy_line3:  
+        enemy.move()
+        time.sleep(1)  
 
+while not win.isClosed():
+    player_spawn.move()
+    win.update()
+    time.sleep(.1)
 
-win.getMouse()
+win.close()
 
+#TO-DO LIST:
+#if car reaches position 500, 500
+#then undraw car and reset position
+    
 #if frog_point == car_point
 #frog dead 
